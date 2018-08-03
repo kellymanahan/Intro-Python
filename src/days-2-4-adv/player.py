74c3318435c445d01f2600cd7742b17a4f1eb364
kammagik <kammagik@yahoo.com>
2018-08-02 19:33:05 -0700

    changed player class

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,5 +1,5 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, room):
-        self.room = room 
+    def __init__(self, location):
+        self.location = rooms[location] 
